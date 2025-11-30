7fb3deabd8d2ada16a937e5a644837f24102512c
Gabriel Sales Dorea <gabriel@jabuticabagames.com.br>
2025-11-30 12:24:42 -0300

    feat: adicionado assets e criado cenas bases

---
+++ Assets/Scripts/CameraTopDown.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class CameraTopDown : MonoBehaviour
+{
+  public Transform target;
+    public float height = 20f;
+    public float smooth = 5f;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.position;
+        offset.y = height;
+    }
+
+    void LateUpdate()
+    {
+        if (!target)return;
+
+        Vector3 desiredPos = target.position + offset;
+        desiredPos.y = height;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, Time.deltaTime * smooth);
+
+    }
+}
